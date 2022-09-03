5fec7db53484661fb21b4951a1904cb313f48f99
Jakob Hansen <jakob.hansen@hotmail.no>
2022-09-03 23:58:46 +0200

    Initial landing page style

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,21 @@ const Home: NextPage = () => {
         <meta name="description" content="Portfolio webiste" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+
+      <main>
+        <div className="flex justify-center">
+          <div className="w-fit">
+            <div>
+              <h1 className="text-2xl font-bold text-textprimary">
+                Jakob Hansen
+              </h1>
+              <h2 className="text-textsecondary">
+                Computer science student at UiO
+              </h2>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
