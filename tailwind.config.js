27e471a0ed08027ba3bc0674cb2ffc700321fc03
Jakob Hansen <jakob.hansen@hotmail.no>
2022-09-03 23:58:28 +0200

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2c2f33",
+        textprimary: "#ffffff",
+        textsecondary: "#99aab5",
+      },
+    },
   },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
