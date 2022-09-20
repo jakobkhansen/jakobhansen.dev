c8f6a6aea42f0fcf89778042afaa217dedfb0a67
Jakob Hansen <jakob.hansen@hotmail.no>
2022-09-20 15:53:59 +0200

    Fix uio image

--- components/timeline/Timeline.tsx
+++ components/timeline/Timeline.tsx
@@ -1,23 +1,20 @@
 import { Divider } from "./Divider";
-import Microsoft from "../../public/Microsoft.svg";
-import Bekk from "../../public/bekk.svg";
-import UiO from "../../public/uio.svg";
 import { Experience } from "./Experience";
 import { ExperienceEvent } from "./ExperienceEvent";
 
 export function Timeline() {
   return (
     <TimelineLayout>
-      <Experience Logo={Microsoft} title={"Microsoft"}>
+      <Experience logo={"/Microsoft.svg"} title={"Microsoft"}>
         <ExperienceEvent text="Summer internship" period="2022" />
         <ExperienceEvent text="Full-time SWE" period="2023" />
       </Experience>
       <Divider />
-      <Experience Logo={Bekk} title={"Bekk"}>
+      <Experience logo={"/bekk.svg"} title={"Bekk"}>
         <ExperienceEvent text="Summer internship" period="2021" />
       </Experience>
       <Divider />
-      <Experience Logo={UiO} title={"University of Oslo"}>
+      <Experience logo={"/uio.svg"} title={"University of Oslo"}>
         <ExperienceEvent text="Bachelor degree" period="2018 - 2021" />
         <ExperienceEvent text="Teaching assistant" period="2020 - 2022" />
         <ExperienceEvent text="Master degree" period="2021 - 2023" />
