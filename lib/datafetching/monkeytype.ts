c69923ed91787b38b415c0ddb078cd9b603370b2
Jakob Hansen <jakob.hansen@hotmail.no>
2022-10-19 16:44:52 +0200

    No data if not ok

--- lib/datafetching/monkeytype.ts
+++ lib/datafetching/monkeytype.ts
@@ -2,6 +2,9 @@ export async function getMonkeytypeUserData(uid: string) {
   const monkeydata = await fetch(
     `https://api.monkeytype.com/users/${uid}/profile`
   );
+  if (!monkeydata.ok) {
+    return {};
+  }
   console.log("await fetch", monkeydata);
   const json = await monkeydata.json();
   console.log("await json", json);
