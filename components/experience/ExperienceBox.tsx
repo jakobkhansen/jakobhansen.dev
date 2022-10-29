44ea2f6d5291ad9ef8f1ecb732788c1c16fe5fdc
Jakob Hansen <jakob.hansen@hotmail.no>
2022-10-29 15:44:26 +0200

    Init experience story

---
+++ components/experience/ExperienceBox.tsx
@@ -0,0 +1,3 @@
+export function ExperienceBox() {
+  return <div>Experience</div>;
+}
---
+++ stories/experience/Experience.stories.tsx
@@ -0,0 +1,10 @@
+import { ExperienceBox } from "../../components/timeline/ExperienceBox";
+
+export const ExperienceStory = () => <ExperienceBox />;
+
+const config = {
+  title: "Experience",
+  component: Experience,
+};
+
+export default config;
