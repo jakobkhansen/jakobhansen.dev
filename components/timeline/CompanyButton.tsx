988e65d6559de22559f3f738c63c29daa4eb7c00
Jakob Hansen <jakobhansen@microsoft.com>
2024-02-10 15:08:44 +0100

    Fix compile

--- components/timeline/CompanyButton.tsx
+++ components/timeline/CompanyButton.tsx
@@ -12,7 +12,9 @@ export const CompanyButton = ({ Logo, logo, title }: Props) => {
       <div className="h-20 w-20 rounded-full bg-stark duration-300 hover:h-36 hover:w-36 md:h-32 md:w-32">
         <div className="flex h-full w-full items-center justify-center">
           {Logo && <Logo className="m-2 text-black" />}
-          {logo && <Image src={logo} alt={title} width={114} height={114} />}
+          {logo && (
+            <Image src={logo} alt={title || ""} width={114} height={114} />
+          )}
         </div>
       </div>
     </div>
