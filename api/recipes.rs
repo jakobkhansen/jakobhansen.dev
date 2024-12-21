f6b82340ab797d1f38b434bca54cda9c4e32584b
Jakob Hansen <jakobhansen@microsoft.com>
2024-12-21 19:40:24 +0100

    First recipe in API

--- api/recipes.rs
+++ api/recipes.rs
@@ -1,4 +1,7 @@
-use serde_json::json;
+use std::{env, fs::read_to_string, path::Path};
+
+use cooklang::{Converter, CooklangParser, Extensions};
+use serde_json::{json, to_string};
 use vercel_runtime::{run, Body, Error, Request, Response, StatusCode};
 
 #[tokio::main]
@@ -7,14 +10,22 @@ async fn main() -> Result<(), Error> {
 }
 
 pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
+    let root = Path::new(env!("CARGO_MANIFEST_DIR"));
+    let path = root.join("data/recipes/chickenparmesan.cook");
+
+    let parser = CooklangParser::new(Extensions::all(), Converter::default());
+    let recipe = match read_to_string(path) {
+        Ok(contents) => parser.parse(&contents).unwrap_output(),
+        Err(error) => {
+            println!("{:?}", error);
+            panic!()
+        }
+    };
+
+    let content = to_string(&recipe).unwrap();
+
     Ok(Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "application/json")
-        .body(
-            json!({
-              "message": "Hello world"
-            })
-            .to_string()
-            .into(),
-        )?)
+        .body(content.into())?)
 }
