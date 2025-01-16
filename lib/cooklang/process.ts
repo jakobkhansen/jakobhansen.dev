c0da0e6791c08f637fe0cef8f2afed25786b5985
Jakob Hansen <jakobhansen@microsoft.com>
2025-01-16 01:18:42 +0100

    Better pre-processing of recipes and order recipes by section

--- lib/cooklang/process.ts
+++ lib/cooklang/process.ts
@@ -1,7 +1,33 @@
 import { Recipe } from "./types";
 
 export function preprocess_recipe(recipe: Recipe): Recipe {
-  return {
-    ...recipe,
+  findIngredientSection(recipe);
+  filterIngredients(recipe);
+  return recipe;
+}
+
+function findIngredientSection(recipe: Recipe) {
+  recipe.sections.forEach((section, secNum) => {
+    section.content.forEach((content) => {
+      const step = content.value;
+      step.items.forEach((item) => {
+        if (item.type === "ingredient") {
+          const ingredient = recipe.ingredients[item.index];
+          ingredient.extra = {
+            ...ingredient.extra,
+            section: secNum,
+          };
+        }
+      });
+    });
+  });
+}
+
+function filterIngredients(recipe: Recipe) {
+  recipe.extra = {
+    ...recipe.extra,
+    filteredIngredients: recipe.ingredients.filter((ingredient) => {
+      return ingredient.modifiers === "";
+    }),
   };
 }
